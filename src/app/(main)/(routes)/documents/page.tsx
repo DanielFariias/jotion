eb6fa82e89c405bd00049cc1956b7c60b06b1333
Daniel Farias <danielfariias15@gmail.com>
2025-01-30 23:55:06 -0300

    feat: add all backend function add test to create a first project

--- src/app/(main)/(routes)/documents/page.tsx
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
 
+import { api } from "@/../convex/_generated/api";
+
 export default function DocumentsPage() {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
 
-  const onCreate = () => {};
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created",
+      error: "Failed to create a new note",
+    });
+  };
 
   return (
     <div className="flex flex-col justify-center items-center h-full space-y-4">

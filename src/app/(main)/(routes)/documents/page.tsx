e53d564cb52e35d3e96667fd392d83878ca7fdeb
Daniel Farias <danielfariias15@gmail.com>
2025-01-30 23:30:40 -0300

    feat: add walcome to documents page

--- src/app/(main)/(routes)/documents/page.tsx
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -1,3 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 export default function DocumentsPage() {
-  return <div>DocumentsPage</div>;
+  const { user } = useUser();
+
+  const onCreate = () => {};
+
+  return (
+    <div className="flex flex-col justify-center items-center h-full space-y-4">
+      <Image
+        className="dark:hidden"
+        src="/empty.png"
+        alt="Empty"
+        width="300"
+        height="300"
+      />
+      <Image
+        className="hidden dark:block"
+        src="/empty-dark.png"
+        alt="Empty"
+        width="300"
+        height="300"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button onClick={onCreate}>
+        <PlusCircle className="w-4 h-4 mr-2" />
+        Create note
+      </Button>
+    </div>
+  );
 }
